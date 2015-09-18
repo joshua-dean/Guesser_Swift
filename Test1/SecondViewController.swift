e4b72319884d2aceb806846f35438e1ee2c14e16
Sam Wendolek <samwendolek@sams-macbook-air.local>
2015-09-18 16:04:55 -0500

    Made the second view controller work, also made a number pad for the second view controller to handle the guesses

--- Test1/SecondViewController.swift
+++ Test1/SecondViewController.swift
@@ -8,6 +8,82 @@
 
 import UIKit
 
-class SecondViewController: NSObject {
+class SecondViewController: UIViewController {
+    
+    var numberguessed = 0
+    var numberlength = 0
+    var numberlabel = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var TheNumber: UILabel!
+    
+    @IBAction func Add0(sender: AnyObject) {
+        numberlabel = numberlabel + "0"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add1(sender: AnyObject) {
+        numberlabel = numberlabel + "1"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add2(sender: AnyObject) {
+        numberlabel = numberlabel + "2"
+        TheNumber.text = numberlabel
+    }
+    
+    
+    @IBAction func Add3(sender: AnyObject) {
+        numberlabel = numberlabel + "3"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add4(sender: AnyObject) {
+        numberlabel = numberlabel + "4"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add5(sender: AnyObject) {
+        numberlabel = numberlabel + "5"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add6(sender: AnyObject) {
+        numberlabel = numberlabel + "6"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add7(sender: AnyObject) {
+        numberlabel = numberlabel + "7"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add8(sender: AnyObject) {
+        numberlabel = numberlabel + "8"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func Add9(sender: AnyObject) {
+        numberlabel = numberlabel + "9"
+        TheNumber.text = numberlabel
+    }
+    
+    @IBAction func DeleteNumber(sender: AnyObject) {
+        if numberlabel.characters.count > 0
+        {
+            numberlabel = numberlabel.substringToIndex(numberlabel.endIndex.predecessor())
+            TheNumber.text = numberlabel
+        }
+    }
 
 }
--- Test1/ViewController.swift
+++ Test1/ViewController.swift
@@ -12,54 +12,15 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    var counter = 0
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        blazeIt.text = "Guesser"
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-
-    /*
-    @IBOutlet weak var blazeIt: UITextField!
-
-    @IBAction func blazeItV2(sender: AnyObject) {
-        blazeIt.text = "420"
-    }
-    */
-    
-    @IBOutlet weak var blazeIt: UILabel!
-    
-    @IBAction func blazeFunk(sender: AnyObject) {
-        if blazeIt.text == "420"
-        {
-            blazeIt.text = "Guesser"
-        }
-        else
-        {
-            blazeIt.text = "420"
-        }
-    }
-    
-    @IBAction func Button2(sender: AnyObject) {
-        blazeIt.text = "Vex"
-    }
-    
-    @IBAction func DaButton(sender: AnyObject) {
-    }
-    
-    @IBOutlet weak var CountingIsFun: UILabel!
-    
-    
-    @IBAction func DaCount(sender: AnyObject) {
-        counter++
-        CountingIsFun.text = "\(counter)"
     }
     
 }
