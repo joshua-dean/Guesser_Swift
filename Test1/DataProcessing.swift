4d627b679c3be881469f80af495c416cba14d926
DEAN, JOSHUA <735582@ehs-s-itc-15.local>
2015-09-18 10:34:23 -0500

    Added Data Storage file

---
+++ Test1/DataProcessing.swift
@@ -0,0 +1,14 @@
+//
+//  DataProcessing.swift
+//  Test1
+//
+//  Created by DEAN, JOSHUA on 9/18/15.
+//  Copyright © 2015 Josh. All rights reserved.
+//
+
+import Foundation
+
+func storeDouble(name: String, value: Double)
+{
+    
+}
