283dd8ac6f1386c0106d917ea2e267c4f5dd77c7
DEAN, JOSHUA <735582@ehs-s-itc-15>
2015-09-21 11:00:12 -0500

    Added shop page

---
+++ Test1/ShopViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ShopViewController.swift
+//  Test1
+//
+//  Created by DEAN, JOSHUA on 9/21/15.
+//  Copyright © 2015 Josh. All rights reserved.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
