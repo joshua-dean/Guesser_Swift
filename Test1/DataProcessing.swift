e222e6f2df15ce72285532423352fe0231c500b9
DEAN, JOSHUA <735582@ehs-s-itc-15>
2015-10-21 10:56:10 -0500

    Documented DataProcessing

--- Test1/DataProcessing.swift
+++ Test1/DataProcessing.swift
@@ -11,12 +11,13 @@ import Foundation
 let defaults = NSUserDefaults.standardUserDefaults()
 let nolevel = -42.0
 
+//Stores a double with a name identifier
 func storeDouble(name: String, value: Double)
 {
     defaults.setValue(String(value), forKey: name);
     defaults.synchronize()
 }
-
+//Retrieves a stored double, using the name it was stored as
 func retrieveDouble(name: String) -> Double?
 {
     if let potatoe = defaults.valueForKey(name)
@@ -25,13 +26,13 @@ func retrieveDouble(name: String) -> Double?
     }
     return nolevel
 }
-
+//Stores a boolean with a name identifier
 func storeBool(name: String, value: Bool)
 {
     defaults.setValue(String(value), forKey: name)
     defaults.synchronize()
 }
-
+//Retrieves a stored boolean, using the name it was stored as
 func retrieveBool(name: String) -> Bool?
 {
     if let tempBool = defaults.valueForKey(name)
@@ -40,7 +41,7 @@ func retrieveBool(name: String) -> Bool?
     }
     return false
 }
-
+//Stores a boolean array with a name identifier
 func storeBoolArray(name: String, valArray: [Bool])
 {
     var tempStore: [NSString]
@@ -52,11 +53,11 @@ func storeBoolArray(name: String, valArray: [Bool])
     defaults.setValue(tempStore, forKey: name)
     defaults.synchronize()
 }
-
+//Retrieves a stored boolean array, using the name it was stored as
 func retrieveBoolArray(name: String) -> [Bool]?
 {
     let tempRet: [NSString]
-    tempRet = defaults.valueForKey(name)! as! [NSString]
+    tempRet = defaults.valueForKey(name)! as! [NSString] //force unwrap to type NSString
     var tempStore: [Bool]
     tempStore = []
     for i in 0...tempRet.count-1
